866757a8a553787f68c8ec60ee1f29afc6654b89
Justin <mrjd225@tiberius.home>
2011-11-16 21:26:43 -0500

    debugging

--- src/SequenceExecuter.java
+++ src/SequenceExecuter.java
@@ -628,9 +628,9 @@ public class SequenceExecuter
 						
 			try 
 			{		
-				//conn.setAutoCommit(false);
+				conn.setAutoCommit(false);
 				
-				//setStatementTimeout(think_time_remaining); 
+				setStatementTimeout(think_time_remaining); 
 				
 				System.out.println("...in prefetch thread...prefetching " + partitions_to_prefetch.size() + " partitions"); 
 				for(int i = 0; i < partitions_to_prefetch.size(); i++)
@@ -651,7 +651,7 @@ public class SequenceExecuter
 					try 
 					{
 						//result = stmt.executeQuery(next_partition.toSQL());
-						result.close();
+						//result.close();
 					}
 					catch(Exception e)
 					{
